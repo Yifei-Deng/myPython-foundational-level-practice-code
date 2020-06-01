020268db4f938a081eaeba55e0f73ba8509199b1
Yifei-Deng <66240809+Yifei-Deng@users.noreply.github.com>
2020-06-01 23:33:05 +0800

    Add files via upload

---
+++ 14(a).py
@@ -0,0 +1,26 @@
+'''
+视频中老师演示的代码
+python开发基础14-变量定义规则 
+
+'''
+student_name="张三"
+print(type(student_name))
+
+student_name=1
+print(type(student_name))
+
+student_name=1.5
+print(type(student_name))
+
+'''
+output:
+
+<class 'str'>                                                                                                           
+<class 'int'>                                                                                                           
+<class 'float>
+
+python在定义变量的时候，不需要去声明变量的类型，变量的类型是根据赋给它的值的类型来决定的
+
+'''
+
+
---
+++ 14(c).py
@@ -0,0 +1,20 @@
+'''
+视频中老师演示的代码
+python开发基础14-变量定义规则 
+
+'''
+import keyword
+print(keyword.kwlist)
+
+'''
+output:
+
+['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', '
+finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'retu
+rn', 'try', 'while', 'with', 'yield'] 
+
+以上单词为python保留关键字，不要使用它们命名,也不要使用内置的方法名（i.e list, if）
+
+'''
+
+
