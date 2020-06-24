5733430fa1158a6a81153ebdab522f12f6ed7b30
Yifei-Deng <66240809+Yifei-Deng@users.noreply.github.com>
2020-06-24 14:10:35 +0800

    Add files via upload

---
+++ 34(b).py
@@ -0,0 +1,63 @@
+'''
+
+python开发基础34-if分支判断结构
+
+'''
+
+if '':
+    print("'' is True.")
+else:
+    print("'' is False.")
+
+if ' ':
+    print("' ' is True.")
+else:
+    print("' ' is False.")
+
+
+if 0:
+    print("0 is True.")
+else:
+    print("0 is False.")
+
+if ():
+    print("() is True.")
+else:
+    print("() is False.")
+
+if []:
+    print("[] is True.")
+else:
+    print("[] is False.")
+
+if {}:
+    print("{} is True.")
+else:
+    print("{} is False.")
+
+if None:
+    print("None is True.")
+else:
+    print("None is False.")
+
+if False:
+    print("False is True.")
+else:
+    print("False is False.")
+
+
+
+
+'''
+Outputs:
+
+'' is False.
+' ' is True.
+0 is False.
+() is False.
+[] is False.
+{} is False.
+None is False.
+False is False.
+
+'''
