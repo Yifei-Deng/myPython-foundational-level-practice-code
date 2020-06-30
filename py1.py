efc04635e4fa098cecc6199c03e5b0020bbdc56f
Yifei-Deng <66240809+Yifei-Deng@users.noreply.github.com>
2020-06-30 15:47:45 +0800

    Add files via upload

---
+++ py1.py
@@ -0,0 +1,12 @@
+'''
+
+python开发基础43-包和模块
+
+'''
+
+a = 1
+
+def fun1(name):
+    print('Hello',name)
+
+
---
+++ py2.py
@@ -0,0 +1,35 @@
+'''
+
+python开发基础43-包和模块
+
+'''
+
+#通过import直接导入模块中所有的变量、函数、类等资源
+import py1
+
+
+print(py1.a)
+
+py1.fun1('World')
+
+
+'''
+Outputs:
+
+1
+Hello World
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
