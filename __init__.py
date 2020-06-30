213c0f6b133e21817258513e763c993b9ce624ec
Yifei-Deng <66240809+Yifei-Deng@users.noreply.github.com>
2020-06-30 16:00:13 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,26 @@
+'''
+
+python开发基础43-包和模块
+
+'''
+
+
+#Packages are namespaces which contain multiple packages and modules themselves. They are simply directories, but with a twist.
+
+#Each package in Python is a directory which MUST contain a special file called __init__.py.
+#This file can be empty, and it indicates that the directory it contains is a Python package
+#So it can be imported the same way a module can be imported.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
