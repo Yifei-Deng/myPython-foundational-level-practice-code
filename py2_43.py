a07b2cc75dd111d3db58f5bf4eccb3ac18a56b82
Yifei-Deng <66240809+Yifei-Deng@users.noreply.github.com>
2020-07-01 22:17:32 +0800

    Add files via upload

---
+++ py2_43.py
@@ -0,0 +1,36 @@
+'''
+
+python开发基础43-包和模块
+
+'''
+
+#通过import直接导入模块中所有的变量、函数、类等资源
+
+import py1_43
+
+
+print(py1_43.a)
+
+py1_43.fun1('World')
+
+
+'''
+Outputs:
+
+1
+Hello World
+
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ py3_43.py
@@ -0,0 +1,29 @@
+'''
+
+python开发基础43-包和模块
+
+'''
+
+#通过from...import...选择性导入指定的资源
+
+from py1_43 import a
+from py1_43 import fun1
+from py1_43 import *            # *导入所有资源
+
+print(a)
+fun1('World')        #避免每次在调用其他模块的变量或函数时，每次都输入包名或模块名
+
+
+
+'''
+Outputs:
+
+1
+Hello World
+
+
+'''
+
+
+
+
