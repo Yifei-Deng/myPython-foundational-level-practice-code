c74e633bbf52b160d0bd5d2135883ad29feeb3ec
Yifei-Deng <66240809+Yifei-Deng@users.noreply.github.com>
2020-07-05 14:07:38 +0800

    Add files via upload

---
+++ 49(a).py
@@ -0,0 +1,47 @@
+'''
+
+python开发基础49-面向对象3大特性之继承
+
+'''
+
+
+class Students:
+
+    def __init__(self,name,age,studentID):
+        self.n = name
+        self.a = age
+        self.i = studentID
+
+    def study(self):
+        print('I am studying Python')
+
+class inheritStudents(Students):
+
+    def school(self):
+        print('I am a student at ABC school')
+
+if __name__ == '__main__':
+    student = inheritStudents('Jane Johnson',35,'M111')
+    print('My name is {}, and I am {} years old'.format(student.n,student.a))
+    student.school()
+    student.study()
+    print('My student number is', student.i)
+
+
+'''
+Outputs:
+
+
+My name is Jane Johnson, and I am 35 years old
+I am a student at ABC school
+I am studying Python
+My student number is M111
+
+
+
+
+'''
+
+
+
+
